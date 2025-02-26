980128ae15c47e7db76b60a232da57678999388a
Shahriyor <baxtiyarovich0102@gmail.com>
2025-02-26 14:31:18 +0500

    delete metodi qo'shildi

--- routes/car.route.js
+++ routes/car.route.js
@@ -6,6 +6,7 @@ router.get("/cars", carController.getCars)
 router.get("/cars/:id", carController.getCarById)
 router.post("/cars",carController.addCar)
 router.put("/cars/:id",carController.updateCar)
+router.delete("/cars/:id",carController.deleteCar)
 
 
 module.exports = router
